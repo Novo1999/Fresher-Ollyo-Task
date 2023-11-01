cba323242be3bb5c5e9d416f943a3223e91121ae
Novo1999 <neptune.navo@gmail.com>
2023-11-01 19:58:41 +0600

    added next and prev button for images

--- src/Components/SingleImage.jsx
+++ src/Components/SingleImage.jsx
@@ -2,17 +2,24 @@ import { createPortal } from "react-dom"
 import createImageSource from "../utils/createImageSource"
 import { useContext } from "react"
 import { GalleryContext } from "../App"
+import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai'
 
 const SingleImage = () => {
- const { currentImage } = useContext(GalleryContext)
-
+ const { currentImage, setCurrentImage, imageIndex } = useContext(GalleryContext)
+ console.log(currentImage, Number(imageIndex[1]))
  // if user clicks on an image show it in a portal
  return (
   <div>
-   {currentImage && createPortal(
+   {currentImage >= 0 && createPortal(
     <dialog id="my_modal_2" className="modal">
      <div className="modal-box bg-white">
+      <button onClick={() => currentImage >= 1 && setCurrentImage(+currentImage - 1)} className="btn btn-circle btn-outline absolute left-2 top-40 min-[425px]:top-52 sm:top-64">
+       <AiOutlineLeft />
+      </button>
       <img src={createImageSource(Number(currentImage))} alt="image" />
+      <button onClick={() => currentImage < Number(imageIndex.length - 1) && setCurrentImage(+currentImage + 1)} className="btn btn-circle btn-outline absolute right-2 top-40  min-[425px]:top-52 sm:top-64">
+       <AiOutlineRight />
+      </button>
       <p className="py-4 text-black">Press ESC key or click outside to close</p>
      </div>
      <form method="dialog" className="modal-backdrop">
